ce6af53a445cf063d8f45afa612358fa111cfc21
fpemud <fpemud@sina.com>
2017-06-04 13:09:07 +0000

    fix

--- lif_ethernet/__init__.py
+++ lif_ethernet/__init__.py
@@ -22,7 +22,7 @@ def get_plugin(name):
 
 class _PluginObject:
 
-    def init2(self, instanceName, cfg, tmpDir, varDir):
+    def init2(self, instanceName, cfg, tmpDir, varDir, firewallAllowFunc):
         assert instanceName == ""
 
     def start(self):
